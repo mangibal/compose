526d6ab2ca91692c7cc61b5764e8066f5518aa7f
Iqbal Fauzi <iqbal.fauzi.if99@gmail.com>
2021-10-16 20:58:27 +0700

    3. Material Design

--- app/src/main/java/com/iqbalfauzi/compose/MainActivity.kt
+++ app/src/main/java/com/iqbalfauzi/compose/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.iqbalfauzi.compose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.iqbalfauzi.compose.ui.theme.ComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,22 +27,79 @@ class MainActivity : ComponentActivity() {
             ComposePlaygroundTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MessageCard(msg = MessageEntity("Android", "Jetpack Compose"))
+//                    Greeting("Android")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+data class MessageEntity(val author: String, val body: String)
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposePlaygroundTheme {
-        Greeting("Android")
+fun MessageCard(msg: MessageEntity) {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.img),
+            contentDescription = "Contact Profile Picture",
+            modifier = Modifier
+                // set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(4.dp)
+                )
+            }
+        }
     }
 }
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = MessageEntity(
+            "Colleague",
+            "Hey, take a look at Jetpack Compose, it's great!"
+        )
+    )
+}
+
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    ComposePlaygroundTheme {
+//        Greeting("Android")
+//    }
+//}
