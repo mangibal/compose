e409ffb4948613e8e083eac9f6b591329410b22b
Iqbal Fauzi <iqbal.fauzi.if99@gmail.com>
2021-10-20 21:29:38 +0700

    State

--- app/src/main/java/com/iqbalfauzi/compose/MainActivity.kt
+++ app/src/main/java/com/iqbalfauzi/compose/MainActivity.kt
@@ -1,21 +1,20 @@
 package com.iqbalfauzi.compose
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.iqbalfauzi.compose.ui.theme.ComposePlaygroundTheme
@@ -27,75 +26,45 @@ class MainActivity : ComponentActivity() {
             ComposePlaygroundTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    MessageCard(msg = MessageEntity("Android", "Jetpack Compose"))
 //                    Greeting("Android")
+                    HelloScreen()
                 }
             }
         }
     }
 }
 
-data class MessageEntity(val author: String, val body: String)
-
+@Preview
 @Composable
-fun MessageCard(msg: MessageEntity) {
-    // Add padding around our message
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(id = R.drawable.img),
-            contentDescription = "Contact Profile Picture",
-            modifier = Modifier
-                // set image size to 40 dp
-                .size(40.dp)
-                // Clip image to be shaped as a circle
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
-        )
+fun HelloScreen() {
+    var name by rememberSaveable { mutableStateOf("") }
 
-        // Add a horizontal space between the image and the column
-        Spacer(modifier = Modifier.width(8.dp))
-
-        Column {
-            Text(
-                text = msg.author,
-                color = MaterialTheme.colors.secondaryVariant,
-                style = MaterialTheme.typography.subtitle2
-            )
-            // Add a vertical space between the author and message texts
-            Spacer(modifier = Modifier.height(4.dp))
-
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
-                Text(
-                    text = msg.body,
-                    style = MaterialTheme.typography.body2,
-                    modifier = Modifier.padding(4.dp)
-                )
-            }
-        }
-    }
+    HelloContent(name = name, onNameChange = { name = it })
 }
 
-@Preview(name = "Light Mode")
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode"
-)
 @Composable
-fun PreviewMessageCard() {
-    MessageCard(
-        msg = MessageEntity(
-            "Colleague",
-            "Hey, take a look at Jetpack Compose, it's great!"
+fun HelloContent(name: String, onNameChange: (String) -> Unit) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        if (name.isNotEmpty()) {
+            Text(
+                text = "Hello, $name!",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.h5
+            )
+        }
+        OutlinedTextField(
+            value = name,
+            onValueChange = onNameChange,
+            label = { Text("Name") }
         )
-    )
+    }
 }
 
 //@Composable
 //fun Greeting(name: String) {
 //    Text(text = "Hello $name!")
 //}
-//
+
 //@Preview(showBackground = true)
 //@Composable
 //fun DefaultPreview() {
